e6a9d51b7bab8d056e9203eb0a040397725fe907
Mufti Faris Murtadho <muftifarisjob@gmail.com>
2026-07-26 03:45:41 +0700

    Update: Support Inno Setup 7 (64-bit)

--- build-installer.cmd
+++ build-installer.cmd
@@ -13,13 +13,13 @@ echo Step 2: Creating installer with Inno Setup...
 echo (Make sure Inno Setup is installed: https://jrsoftware.org/isinfo.php)
 echo.
 
-if not exist "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" (
+if not exist "C:\Program Files\Inno Setup 7\ISCC.exe" (
     echo ERROR: Inno Setup not found!
     echo Please install from: https://jrsoftware.org/isinfo.php
     goto :error
 )
 
-"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" installer.iss
+"C:\Program Files\Inno Setup 7\ISCC.exe" installer.iss
 if %errorlevel% neq 0 goto :error
 
 echo.
